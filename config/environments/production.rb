4fd88ac90e3ef2f4659600a7b0800c0482a8240d
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-12 23:18:22 +0000

    Deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,6 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # Use a different logger for distributed setups.
-  # require 'syslog/logger'
-  # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
   if ENV["RAILS_LOG_TO_STDOUT"].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
@@ -91,4 +88,13 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['victor.silva@soulasalle.com.br'],
+ :password             => ENV['Fatima10'],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 end

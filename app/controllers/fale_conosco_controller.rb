014e629c002fdf9450e21b1a642e53cd300525ec
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-13 17:43:05 +0000

    Revert "Build 0.0.1"
    This reverts commit 0b58895f059d234e4574fd44f3638c24f4cee889.

---
+++ app/controllers/fale_conosco_controller.rb
@@ -0,0 +1,2 @@
+class FaleConoscoController < ApplicationController
+end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,5 +1,6 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
@@ -66,14 +67,14 @@ Rails.application.configure do
 # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 # config.action_mailer.delivery_method = :smtp
 # SMTP settings for gmail
-#config.action_mailer.smtp_settings = {
-# :address              => "smtp.gmail.com",
-# :port                 => 587,
-# :user_name            => ENV['victor.silva@soulasalle.com.br'],
-# :password             => ENV[''],
-# :authentication       => "plain",
-#:enable_starttls_auto => true
-#}
+config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['victor.silva@soulasalle.com.br'],
+ :password             => ENV[''],
+ :authentication       => "plain",
+:enable_starttls_auto => true
+}
 
 # Send email in development mode?
 config.action_mailer.perform_deliveries = true

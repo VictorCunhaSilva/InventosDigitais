23cd7b27b02ad8dd60779a9982d8414fa5bfe51b
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-14 20:52:50 +0000

    Email Credentials

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,8 +92,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => "vcunha365@gmail.com",
- :password             => "Fatima10",
+ :user_name            => "minion.store.online@gmail.com",
+ :password             => "Minion.Store10",
  :authentication       => "plain",
  :enable_starttls_auto => true
 }

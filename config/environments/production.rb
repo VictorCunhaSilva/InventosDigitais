b13c0647b686de51394b06a27c438aab27eaf232
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-14 17:16:00 +0000

    SMTP - Test Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,9 +92,9 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
  :address              => "smtp.gmail.com",
  :port                 => 587,
- :user_name            => ENV['vcunha365@gmail.com'],
- :password             => ENV['Fatima10'],
- :authentication       => "login",
+ :user_name            => "vcunha365@gmail.com",
+ :password             => "Fatima10",
+ :authentication       => "plain",
  :enable_starttls_auto => true
 }
 end

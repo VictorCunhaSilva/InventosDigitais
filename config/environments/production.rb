fa66c69f8ba988070207745e3a0d09594418f96c
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-14 16:30:55 +0000

    Email Config - SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,13 +89,12 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   
-  config.action_mailer.delivery_method = :letter_opener
-#  config.action_mailer.smtp_settings = {
-# :address              => "smtp.gmail.com",
-# :port                 => 587,
-# :user_name            => ENV['victor.silva@soulasalle.com.br'],
-# :password             => ENV['Fatima10'],
-# :authentication       => "plain",
-# :enable_starttls_auto => true
-#}
+  config.action_mailer.smtp_settings = {
+ :address              => "smtp.gmail.com",
+ :port                 => 587,
+ :user_name            => ENV['vcunha365@gmail.com'],
+ :password             => ENV['Fatima10'],
+ :authentication       => "auth",
+ :enable_starttls_auto => true
+}
 end

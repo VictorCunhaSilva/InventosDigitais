2967f61c45f10db6cecc19c5cf5de60e80118430
Victor Cunha <victor.silva@soulasalle.com.br>
2019-04-14 16:33:01 +0000

    SMTP - Email Config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -94,7 +94,7 @@ Rails.application.configure do
  :port                 => 587,
  :user_name            => ENV['vcunha365@gmail.com'],
  :password             => ENV['Fatima10'],
- :authentication       => "auth",
+ :authentication       => "plain",
  :enable_starttls_auto => true
 }
 end
